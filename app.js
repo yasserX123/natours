713af39285ca8647e003e870e61adafe71f7e3ae
Yasser <trabulsiyasser@gmail.com>
2024-11-26 11:30:29 -0600

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -29,6 +30,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Global MIDDLEWARE
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// api.use(cors({ //to allow specific website
+//  origin: 'https://www.natours.com'
+//}))
+
+app.options('*', cors()); // to allow all types of http requests (PUT, PATCH, DELETE)
+
+// app.options('/api/v1/tours/:id', cors());
+
 //Set security HTTP Headers
 /* I turned it off because it's not allowing parcel to work
 app.use(
